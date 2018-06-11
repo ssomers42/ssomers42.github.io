e631911d654c5c2d6eb017c14e3ee9b2698f87eb
Stephan <ssomers42@gmail.com>
2018-06-11 09:55:01 -0700

    fixed now?

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,30 @@
 
-// $(document).ready(function(){
-//     var speed = 1000;
+$(document).ready(function(){
+    // var speed = 1000;
 
-//     // check for hash and if div exist... scroll to div
-//     var hash = window.location.hash;
-//     if($(hash).length) scrollToID(hash, speed); 
+    // // check for hash and if div exist... scroll to div
+    // var hash = window.location.hash;
+    // if($(hash).length) scrollToID(hash, speed); 
 
-//     // scroll to div on nav click
-//     $('.scroll-link').click(function (e) {
-//         e.preventDefault();
-//         var id = $(this).attr('href');
-//         if($(id ).length) scrollToID(id, speed);
-//     });
-// })
+    // // scroll to div on nav click
+    // $('.scroll-link').click(function (e) {
+    //     e.preventDefault();
+    //     var id = $(this).attr('href');
+    //     if($(id ).length) scrollToID(id, speed);
+    // });
 
-// function scrollToID(id, speed) {
-//     var offSet = 70;
-//     var obj = $(id).offset();
-//     var targetOffset = obj.top - offSet;
-//     $('html,body').animate({ scrollTop: targetOffset }, speed);
-// }
+    // function scrollToID(id, speed) {
+    //     var offSet = 70;
+    //     var obj = $(id).offset();
+    //     var targetOffset = obj.top - offSet;
+    //     $('html,body').animate({ scrollTop: targetOffset }, speed);
+    // }
 
-$("#workNav").click(function() {
-	$('html,body').animate({
-	    scrollTop: $(".myWork").offset().top},
-	    'slow');
-});
+    $("#workNav").click(function() {
+    	$('html,body').animate({
+    	    scrollTop: $(".myWork").offset().top},
+    	    'slow');
+    });
+})
+
+
