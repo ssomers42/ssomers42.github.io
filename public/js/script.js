8f4f8a8b13d3e5ccaecb47c2aa68de9fed3edb2b
Stephan <ssomers42@gmail.com>
2018-06-11 09:38:30 -0700

    scroll/redirect

---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    // Read the cookie and if it's defined scroll to id
+    var scroll = Cookies.get('scroll');
+    if(scroll){
+        scrollToID(scroll, 1000);
+        $.removeCookie('scroll');
+    }
+
+    // Handle event onclick, setting the cookie when the href != #
+    $('.nav a').click(function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var href = $(this).attr('href');
+        if(href === '#'){
+            scrollToID(id, 1000);
+        }else{
+            Cookies.get('scroll', id);
+            window.location.href = href;
+        }
+    });
+
+    // scrollToID function
+    function scrollToID(id, speed) {
+        var offSet = 70;
+        var obj = $('#' + id);
+        if(obj.length){
+          var offs = obj.offset();
+          var targetOffset = offs.top - offSet;
+          $('html,body').animate({ scrollTop: targetOffset }, speed);
+        }
+    }
+    
+    $("#workNav").click(function() {
+    	$('html,body').animate({
+    	    scrollTop: $(".myWork").offset().top},
+    	    'slow');
+    });
+});
