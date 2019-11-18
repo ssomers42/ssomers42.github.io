f1c67402adf20d3944802b7a7cd60297a1c8c2db
Stephan Somers <stephan@stephans-mbp.home>
2019-11-17 21:04:23 -0800

    Minor cleanup. Scroll added (WiP)

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,24 @@
 
 $(document).ready(function(){
-    // var speed = 1000;
+    var speed = 1000;
 
-    // // check for hash and if div exist... scroll to div
-    // var hash = window.location.hash;
-    // if($(hash).length) scrollToID(hash, speed); 
+    // check for hash and if div exist... scroll to div
+    var hash = window.location.hash;
+    if($(hash).length) scrollToID(hash, speed); 
 
-    // // scroll to div on nav click
-    // $('.scroll-link').click(function (e) {
-    //     e.preventDefault();
-    //     var id = $(this).attr('href');
-    //     if($(id ).length) scrollToID(id, speed);
-    // });
+    // scroll to div on nav click
+    $('.scroll-link').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href');
+        if($(id ).length) scrollToID(id, speed);
+    });
 
-    // function scrollToID(id, speed) {
-    //     var offSet = 70;
-    //     var obj = $(id).offset();
-    //     var targetOffset = obj.top - offSet;
-    //     $('html,body').animate({ scrollTop: targetOffset }, speed);
-    // }
+    function scrollToID(id, speed) {
+        var offSet = 70;
+        var obj = $(id).offset();
+        var targetOffset = obj.top - offSet;
+        $('html,body').animate({ scrollTop: targetOffset }, speed);
+    }
 
     $("#workNav").click(function() {
     	$('html,body').animate({
